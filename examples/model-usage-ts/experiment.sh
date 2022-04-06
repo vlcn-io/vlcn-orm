c5ef7c862baca3b745460f4090abb97d69628910
Matt <matt.wonlaw@gmail.com>
2022-04-06 11:44:13 -0400

    add turbo watch command

--- examples/model-usage-ts/experiment.sh
+++ examples/model-usage-ts/experiment.sh
@@ -2,4 +2,4 @@
 # first one is relative to cli.js location
 # second is relative to where script is invoked
 # both should be relative to script invoke location
-node ./node_modules/@strut/model/lib/cli.js gen ./lib/example/domain.aphro --dest ./src/example/generated
+node ./node_modules/@aphro/codegen/lib/cli.js gen ./src/example/domain.aphro --dest ./src/example/generated
