d69d42da77a7a65ed2c8cc3f7747bff89716c96a
Matt <1009003+tantaman@users.noreply.github.com>
2022-04-14 10:57:18 -0400

    add method to configure aphrodite with extensions

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -6,6 +6,6 @@ export { default as fieldFn } from './field.js';
 export * from './module.js';
 import { GrammarExtension } from '@aphro/grammar-extension-api';
 
-export function congiure({ extensions }: { extensions: GrammarExtension[] }) {
+export function congiure({ extensions }: { extensions: GrammarExtension<any, any>[] }) {
   // Augment our parser with the provided grammar extensions
 }
