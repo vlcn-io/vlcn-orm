356feb409d0edb9bf27ceb6b27d17ed044962d79
Matt <1009003+tantaman@users.noreply.github.com>
2022-05-13 16:59:58 -0400

    add subscription support to models

--- packages/model-runtime-ts/src/Model.ts
+++ packages/model-runtime-ts/src/Model.ts
@@ -1,13 +1,20 @@
 import { SID_of } from '@strut/sid';
+import { Disposer } from '@strut/events';
 import { NodeSpec } from '@aphro/schema-api';
 import { Context } from '@aphro/context-runtime-ts';
 
 export interface IModel<T extends {}> {
   readonly id: SID_of<this>;
   readonly context: Context;
+
+  subscribe(c: () => void): Disposer;
+  subscribeTo(keys: (keyof T)[], c: () => void): Disposer;
+
   // Internal only APIs. Exposed since TS doesn't understand package friends.
   _get<K extends keyof T>(key: K): T[K];
   _d(): T;
+
+  destroy();
 }
 
 export type ModelSpec<M extends IModel<D>, D extends {}> = {
@@ -28,6 +35,9 @@ export default abstract class Model<T extends {}> implements IModel<T> {
 
   protected readonly data: T;
 
+  private subscriptions: Set<() => void> = new Set();
+  private keyedSubscriptions: Map<keyof T, Set<() => void>> = new Map();
+
   constructor(context: Context, data: T) {
     this.context = context;
     this.data = Object.freeze(data);
@@ -36,8 +46,31 @@ export default abstract class Model<T extends {}> implements IModel<T> {
   _get<K extends keyof T>(key: K): T[K] {
     return this.data[key];
   }
-
   _d(): T {
     return this.data;
+  }
+
+  subscribe(c: () => void): Disposer {
+    this.subscriptions.add(c);
+    return () => this.subscriptions.delete(c);
+  }
+
+  subscribeTo(keys: (keyof T)[], c: () => void): Disposer {
+    keys.forEach(k => {
+      let subs = this.keyedSubscriptions.get(k);
+      if (subs == null) {
+        subs = new Set();
+        this.keyedSubscriptions.set(k, subs);
+      }
+
+      subs.add(c);
+    });
+
+    return () => keys.forEach(k => this.keyedSubscriptions.get(k)?.delete(c));
+  }
+
+  destroy() {
+    this.subscriptions = new Set();
+    this.keyedSubscriptions = new Map();
   }
 }
