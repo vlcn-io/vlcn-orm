9f64f1926e5ef7ba5a7438b9f40d8474c90cfd40
Matt <1009003+tantaman@users.noreply.github.com>
2022-05-14 14:07:18 -0400

    bring over sqlWriter from aphro-p2p

--- packages/mutator-runtime-ts/src/sql/sqlWriter.ts
+++ packages/mutator-runtime-ts/src/sql/sqlWriter.ts
@@ -1,33 +1,28 @@
-// import { Context } from '@aphro/context-runtime-ts';
-// import { IModel } from '@aphro/model-runtime-ts';
-// import { nullthrows } from '@strut/utils';
+import { Context } from '@aphro/context-runtime-ts';
+import { IModel } from '@aphro/model-runtime-ts';
+import { DeleteChangeset } from '../Changeset';
 
-// export default {
-//   // Precondition: already grouped by db & table
-//   // TODO: Should we grab all by DB so we can do many inserts in 1 statement to the
-//   // same db?
-//   async upsertGroup(ctx: Context, nodes: IModel<Object>[]): Promise<void> {
-//     const first = nodes[0];
-//     const persist = nullthrows(first._definition.schema.storage.persisted);
+export default {
+  // Precondition: already grouped by db & table
+  // TODO: Should we grab all by DB so we can do many inserts in 1 statement to the
+  // same db?
+  async upsertGroup(ctx: Context, nodes: IModel<Object>[]): Promise<void> {
+    const first = nodes[0];
+    const spec = first.spec;
+    const persist = spec.storage;
 
-//     // TODO Can we make the db resolver return more richly typed things?
-//     // So we can do stuff in native Knex rather than having to
-//     // go to a query string and then to knex?
-//     const db = context.dbResolver
-//       .type(storageType(persist.engine))
-//       .engine(persist.engine)
-//       .db(persist.db);
+    const db = ctx.dbResolver.type(persist.type).engine(persist.engine).db(persist.db);
 
-//     // Ideally the above would return knex...
-//     // so we don't have to re-construct knex below just for query building...
+    await db(persist.tablish)
+      .insert(nodes.map(n => (n as any)._data))
+      .onConflict(spec.primaryKey)
+      .merge();
+  },
 
-//     await db(persist.tablish)
-//       .insert(nodes.map(n => (n as any)._data))
-//       .onConflict('_id')
-//       .merge();
-//   },
+  async deleteGroup(
+    context: Context,
+    delets: DeleteChangeset<IModel<Object>, Object>[],
+  ): Promise<void> {},
 
-//   async deleteGroup(context: Context, delets: DeleteChangeset<NodeSchema>[]): Promise<void> {},
-
-//   async createTables(): Promise<void> {},
-// };
+  async createTables(): Promise<void> {},
+};
