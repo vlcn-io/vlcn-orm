89f96a518c1978bf248ca74a294e6c70c629f576
Matt <1009003+tantaman@users.noreply.github.com>
2022-06-01 09:32:25 -0400

    rm browser-sqlite given that todomvc is now this example

--- examples/browser-sqlite/src/generated/Todo.sqlite.sql
+++
@@ -1,9 +0,0 @@
--- SIGNED-SOURCE: <9fd8caa2868fcc71aa0da1cec7b129bb>
-create table if not exists ` todo ` (
-  ` id ` bigint,
-  ` text ` varchar(255),
-  ` created ` bigint,
-  ` modified ` bigint,
-  ` ownerId ` bigint,
-  primary key (` id `)
-)
--- examples/browser-sqlite/src/generated/User.sqlite.sql
+++
@@ -1,8 +0,0 @@
--- SIGNED-SOURCE: <4f301f575722ffcd89bedeb073ef1417>
-create table if not exists ` user ` (
-  ` id ` bigint,
-  ` name ` varchar(255),
-  ` created ` bigint,
-  ` modified ` bigint,
-  primary key (` id `)
-)
