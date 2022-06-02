5360fa7ebb3aaa949ea13c24160e79e1b88f5d10
Matt <1009003+tantaman@users.noreply.github.com>
2022-06-02 16:11:45 -0400

    react hooks to integrate with reactivity

--- packages/query-runtime-ts/src/index.ts
+++ packages/query-runtime-ts/src/index.ts
@@ -1,5 +1,5 @@
 export { default as P, Predicate } from './Predicate.js';
-export { DerivedQuery, UpdateType } from './Query.js';
+export { Query, DerivedQuery, UpdateType } from './Query.js';
 export { default as QueryFactory } from './QueryFactory.js';
 export * from './Expression.js';
 export * from './Field.js';
--- packages/react/src/Hooks.ts
+++ packages/react/src/Hooks.ts
@@ -1,29 +1,71 @@
 // TODO: this should be in a separate package from the core model code.
-import { useEffect, useReducer } from 'react';
-import { IModel } from '@aphro/context-runtime-ts';
+import { useEffect, useReducer, useRef, useState } from 'react';
+import { IModel, Query } from '@aphro/runtime-ts';
 import counter from '@strut/counter';
 
-// TODO: use the new react18 external state hook thinger.
 const count = counter('model-infra/Hooks');
-export function useSubscription<T extends IModel<any>>(m: T): T {
+
+export function useBind<M extends IModel<D>, D>(m: M): void;
+export function useBind<M extends IModel<D>, D>(m: M, keys: (keyof D)[]): void;
+
+export function useBind<M extends IModel<D>, D>(m: M, keys?: (keyof D)[]): void {
+  count.bump('useBind.' + m.constructor.name);
+  // TODO swap out to reac18 "useSyncExternalStore"
+  // https://reactjs.org/docs/hooks-reference.html#usesyncexternalstore
   const [tick, forceUpdate] = useReducer(x => x + 1, 0);
   useEffect(() => {
-    count.bump('useSubscription.' + m.constructor.name);
-    // subscribe returns a function which will dispose of the subscription
-    // TODO: uncomment once we fix the models back up
-    // return m.subscribe(() => forceUpdate());
+    if (keys != null) {
+      count.bump('keyed.subscription.' + m.constructor.name);
+      // subscribe returns a function which will dispose of the subscription
+      return m.subscribeTo(keys, () => forceUpdate());
+    } else {
+      // subscribe returns a function which will dispose of the subscription
+      return m.subscribe(() => forceUpdate());
+    }
   }, [m]);
-
-  return m;
 }
 
-export function useQuery<T>(keys: (keyof T)[], m: IModel<T>): void {
-  const [tick, forceUpdate] = useReducer(x => x + 1, 0);
-  count.bump('useQuery.' + m.constructor.name);
+type UseQueryData<T> =
+  | {
+      status: 'loading';
+    }
+  | {
+      status: 'ready';
+      data: T[];
+    }
+  | {
+      status: 'error';
+      error: Error;
+    };
+
+export function useQuery<M extends IModel, Q extends Query<M>>(
+  queryProvider: () => Q,
+  deps: any[],
+): void {
+  const currentLiveResult = useRef();
+  const [result, setResult] = useState<UseQueryData<M>>({
+    status: 'loading',
+  });
+
   useEffect(() => {
-    count.bump('keyed.subscription.' + m.constructor.name);
-    // subscribe returns a function which will dispose of the subscription
-    // TODO: uncomment once we fix the models back up
-    // return m.subscribeTo(keys, () => forceUpdate());
-  }, [m]);
+    const q = queryProvider();
+    const liveResult = q.live();
+    currentLiveResult.current = liveResult;
+    // returns a disposer
+    return q.live().subscribe((data: M[]) => {
+      // this can mismatch if this is an old subscriber from a prior run
+      // of `useEffect`. Theoretically this should not happen.
+      if (liveResult !== currentLiveResult.current) {
+        count.bump('liveresult!==currentLiveResult');
+        return;
+      }
+
+      setResult({
+        status: 'ready',
+        data,
+      });
+    });
+  }, deps);
+
+  return result;
 }
