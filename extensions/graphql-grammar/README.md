418863b69e607f20bf55474321abb07a0fd52c40
Matt <1009003+tantaman@users.noreply.github.com>
2022-06-13 13:14:55 -0400

    Start working on the GraphQL grammar extension

---
+++ extensions/graphql-grammar/README.md
@@ -0,0 +1,3 @@
+# GraphQL Grammar
+
+Extends the `Aphrodite` grammar with support for exposing fields and edges of a node to `GraphQL`.
---
+++ extensions/graphql-grammar/src/index.ts
@@ -0,0 +1,2 @@
+// import { TypeAtom, ValidationError } from '@aphro/schema-api';
+// import { GrammarExtension } from '@aphro/grammar-extension-api';
