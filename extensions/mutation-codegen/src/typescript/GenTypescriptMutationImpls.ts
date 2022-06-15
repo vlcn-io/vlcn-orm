94f1110c840bf997826535abe3269d73ed94f86f
Matt <1009003+tantaman@users.noreply.github.com>
2022-06-15 10:13:41 -0400

    default to throwing on unimplemented mutations

--- extensions/mutation-codegen/src/typescript/GenTypescriptMutationImpls.ts
+++ extensions/mutation-codegen/src/typescript/GenTypescriptMutationImpls.ts
@@ -79,6 +79,7 @@ ${this.getCode([])}
       // e.g., mutator.set({name: "Foo" });
       // You do not need to return anything from this method. The mutator will track your changes.
       // If you do return changesets, those changesets will be applied in addition to the changes made to the mutator.
+      throw new Error('You must implement the mutation ${m.name} for schema ${this.schema.name} in ${this.schema.name}MutationsImpl.ts');
     }`;
   }
 
