a536dead2fcaf5dac92bee7bf66e949ec55e07be
Matt <1009003+tantaman@users.noreply.github.com>
2022-06-19 11:56:41 -0400

    start incorporating a test for sqljs connector but...
    https://github.com/sql-js/sql.js/issues/517

---
+++ packages/sqljs-connector/src/__tests__/connection.test.ts
@@ -0,0 +1,15 @@
+// import { sql } from '@aphro/sql-ts';
+// import initSqlJs from '@aphro/sql.js/dist/sql-wasm.js';
+// import { Connection } from '../index.js';
+// // import * as fs from 'fs';
+// // const filebuffer = fs.readFileSync('./test.sqlite');
+
+// initSqlJs().then(function (SQL) {
+//   // const conn = new Connection(new SQL.Database());
+//   // conn.query(sql`CREATE TABLE "foo" ( "id" bigint NOT NULL )`);
+//   // conn.query(sql`INSERT INTO "foo" (${sql.value(1)})`);
+//   // const result = conn.query(sql`SELECT * FROM "foo"`);
+//   // console.log(result);
+// });
+
+test('see https://github.com/sql-js/sql.js/issues/517', () => {});
