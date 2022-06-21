ca0978b61592ad0afee75303021ef11f60d14751
Matt <1009003+tantaman@users.noreply.github.com>
2022-06-21 09:09:08 -0400

    move todo-mvc to own repo

--- examples/todo-mvc/src/generated/TodoList.sqlite.sql
+++
@@ -1,8 +0,0 @@
--- SIGNED-SOURCE: <aba395c4f02a46c588bb67fd28bd8949>
-CREATE TABLE
-  IF NOT EXISTS "todolist" (
-    "id" bigint NOT NULL,
-    "filter" varchar(255) NOT NULL,
-    "editing" bigint,
-    primary key ("id")
-  )
