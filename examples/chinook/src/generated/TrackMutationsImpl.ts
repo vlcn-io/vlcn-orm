54f055c0123dbf500112ccfa84c2a53a2fe29091
Matt <1009003+tantaman@users.noreply.github.com>
2022-06-23 16:18:16 -0400

    impl track mutation

--- examples/chinook/src/generated/TrackMutationsImpl.ts
+++ examples/chinook/src/generated/TrackMutationsImpl.ts
@@ -1,27 +1,23 @@
-import { CreateArgs } from "./TrackMutations.js";
-import { Changeset } from "@aphro/runtime-ts";
-import { Data } from "./Track.js";
-import Track from "./Track.js";
-import { IMutationBuilder } from "@aphro/runtime-ts";
+import { CreateArgs } from './TrackMutations.js';
+import { Changeset } from '@aphro/runtime-ts';
+import { Data } from './Track.js';
+import Track from './Track.js';
+import { IMutationBuilder, sid } from '@aphro/runtime-ts';
+import deviceId from '../deviceId.js';
 
 export function createImpl(
-  mutator: Omit<IMutationBuilder<Track, Data>, "toChangeset">,
-  {
+  mutator: Omit<IMutationBuilder<Track, Data>, 'toChangeset'>,
+  { name, album, mediaType, genre, composer, milliseconds, bytes, unitPrice }: CreateArgs,
+): void | Changeset<any>[] {
+  mutator.set({
+    id: sid(deviceId),
     name,
-    album,
-    mediaType,
-    genre,
+    albumId: album.id,
+    mediaTypeId: mediaType?.id,
+    genreId: genre?.id,
     composer,
     milliseconds,
     bytes,
     unitPrice,
-  }: CreateArgs
-): void | Changeset<any>[] {
-  // Use the provided mutator to make your desired changes.
-  // e.g., mutator.set({name: "Foo" });
-  // You do not need to return anything from this method. The mutator will track your changes.
-  // If you do return changesets, those changesets will be applied in addition to the changes made to the mutator.
-  throw new Error(
-    "You must implement the mutation create for schema Track in TrackMutationsImpl.ts"
-  );
+  });
 }
