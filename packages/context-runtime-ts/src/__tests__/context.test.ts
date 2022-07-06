a9841addbfe1583d5117dccbb1046b2855cdc115
Matt <1009003+tantaman@users.noreply.github.com>
2022-07-06 10:19:17 -0400

    test print resolver

---
+++ packages/context-runtime-ts/src/__tests__/context.test.ts
@@ -0,0 +1,29 @@
+import { asId } from '@strut/sid';
+import { debugContext } from '../context.js';
+import { viewer } from '../viewer.js';
+import { jest } from '@jest/globals';
+
+test('debug context intercepts and prints all calls to the db', () => {
+  const ctx = debugContext(viewer(asId('sdf')));
+
+  const db = ctx.dbResolver.engine('memory').db('test');
+  expect(db).not.toBeUndefined();
+
+  console.log = jest.fn();
+  db.query({
+    type: 'read',
+    tablish: 'foo',
+    roots: [],
+  });
+  expect(console.log).toBeCalledTimes(2);
+  // expect(console.log).nthCalledWith(1, ['query']);
+  // expect(console.log).nthCalledWith(2, [
+  //   [
+  //     {
+  //       type: 'read',
+  //       tablish: 'foo',
+  //       roots: [],
+  //     },
+  //   ],
+  // ]);
+});
