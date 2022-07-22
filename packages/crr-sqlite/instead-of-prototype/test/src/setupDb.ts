573634ae1b05a10a9bb27ad960ab0cf6cc3db7e9
Matt <1009003+tantaman@users.noreply.github.com>
2022-07-22 11:38:49 -0400

    debuggable db setup

--- packages/crr-sqlite/instead-of-prototype/test/src/setupDb.ts
+++ packages/crr-sqlite/instead-of-prototype/test/src/setupDb.ts
@@ -12,16 +12,21 @@ export default async function setupDb(): Promise<DatabaseConnection> {
 
   // Process schemas serially. Some depend on one another
   // and not a big enough deal to parallelize those that do not.
-  await db.query(sql.file('../schemas/crr_db_version.sqlite.sql'));
-  await db.query(sql.file('../schemas/crr_peer_id.sqlite.sql'));
-  await db.query(sql.file('../schemas/1_todo_crr.sqlite.sql'));
-  await db.query(sql.file('../schemas/2_todo_view.sqlite.sql'));
-  await db.query(sql.file('../schemas/3_insert_todo_trig.sqlite.sql'));
-  await db.query(sql.file('../schemas/4_update_todo_trig.sqlite.sql'));
-  await db.query(sql.file('../schemas/5_delete_todo_trig.sqlite.sql'));
-  await db.query(sql.file('../schemas/6_todo_patch.sqlite.sql'));
-  await db.query(sql.file('../schemas/7_insert_todo_patch.sqlite.sql'));
-  await db.query(sql.file('../schemas/8_todo_vector_clocks.sqlite.sql'));
+  await runfile(db, 'crr_db_version.sqlite.sql');
+  await runfile(db, 'crr_peer_id.sqlite.sql');
+  await runfile(db, '1_todo_crr.sqlite.sql');
+  await runfile(db, '2_todo_view.sqlite.sql');
+  await runfile(db, '3_insert_todo_trig.sqlite.sql');
+  await runfile(db, '4_update_todo_trig.sqlite.sql');
+  await runfile(db, '5_delete_todo_trig.sqlite.sql');
+  await runfile(db, '6_todo_patch.sqlite.sql');
+  await runfile(db, '7_insert_todo_patch.sqlite.sql');
+  await runfile(db, '8_todo_vector_clocks.sqlite.sql');
 
   return db;
+}
+
+async function runfile(db: DatabaseConnection, file: string) {
+  console.log('Running: ' + file);
+  await db.query(sql.file('../schemas/' + file));
 }
