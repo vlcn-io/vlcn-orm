012ff336873907862e4bc4522f1821b8020a65d6
Matt <1009003+tantaman@users.noreply.github.com>
2022-08-10 07:06:14 -0400

    fixup mvc mem example -- missing react type imports

--- examples/todo-mvc-mem/src/index.tsx
+++ examples/todo-mvc-mem/src/index.tsx
@@ -2,7 +2,11 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.js';
 
-const root = createRoot(document.getElementById('container'));
+const container = document.getElementById('container');
+if (container == null) {
+  throw new Error();
+}
+const root = createRoot(container);
 root.render(
   <React.Suspense fallback={<div>Loading...</div>}>
     <App />
