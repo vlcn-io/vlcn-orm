51dbee83ddaf7d46d0946e092faca1bf3b291792
Matt <1009003+tantaman@users.noreply.github.com>
2022-08-23 09:23:31 -0400

    questions on @databases mutex class

--- connectors/absurd-sql-connector/src/main/Mutex.ts
+++ connectors/absurd-sql-connector/src/main/Mutex.ts
@@ -1,3 +1,5 @@
+// From: https://github.com/ForbesLindesay/atdatabases/blob/f62a1bcb152634259edc912b36c59da33a4899ae/packages/sqlite/src/Mutex.ts
+
 interface Task {
   start: number;
   write: boolean;
@@ -29,6 +31,11 @@ export default class Mutex {
   };
   async readLock<T>(fn: () => Promise<T>): Promise<T> {
     const now = Date.now();
+    // TODO: test that we never have to actually wait till timeout
+    // also.. doesn't this conditional look wrong?
+    // if we're running a write or some write has timed out? Should it not be:
+    // if we're running a write or no write has timed out?
+    // also -- why this strange boolean on `runningWrite` that doesn't actually represent if we're running a write?
     if (this.runningWrite || this.tasks.some(t => t.write && now - t.start > this.maxWaitTimeout)) {
       return new Promise((resolve, reject) => {
         this.tasks.push({
